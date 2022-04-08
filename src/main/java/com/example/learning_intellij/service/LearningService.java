e2b17a538c5a910f083c145fc1aba6ea9e196c66
shivam <shivamguptaluck@gmail.com>
2022-04-08 19:43:23 +0530

    Initial Commit

---
+++ src/main/java/com/example/learning_intellij/service/LearningService.java
@@ -0,0 +1,15 @@
+package com.example.learning_intellij.service;
+
+public class LearningService {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+
+    public void runFast()
+    {
+        System.out.println("hello world runFast");
+    }
+
+}
